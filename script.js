1f115434754f73c920e5b95acb5962014bb6802c
afzalafzql11-max <afzalafzql11@gmail.com>
2026-03-25 00:26:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -163,8 +163,5 @@ function toggleMenu(){
   }
 }
 
-function showPage(page){
-  document.querySelectorAll(".page").forEach(p=>p.style.display="none");
-  document.getElementById(page).style.display="block";
-}
+
 showPage("login");
